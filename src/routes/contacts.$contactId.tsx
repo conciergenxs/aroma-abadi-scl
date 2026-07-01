d8500ff752910802069fb4654832ede2673a9d57
Concierge NXS <concierge@nxs.id>
2026-07-01 14:32:43 +0700

    auto: src/routes/contacts.$contactId.tsx [autodeploy]

--- src/routes/contacts.$contactId.tsx
+++ src/routes/contacts.$contactId.tsx
@@ -14,6 +14,7 @@ import {
   Activity as ActivityIcon,
   Image as ImageIcon,
   User2,
+  ShoppingBag,
 } from "lucide-react";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { toast } from "sonner";
