a909fc11c228db6e3540238e76502384cb10baed
Concierge NXS <concierge@nxs.id>
2026-07-06 18:28:33 +0700

    auto: src/routes/templates.index.tsx [autodeploy]

--- src/routes/templates.index.tsx
+++ src/routes/templates.index.tsx
@@ -221,6 +221,7 @@ function TemplatesPage() {
                   <th className="px-4 py-3 text-left font-medium">Channel</th>
                   <th className="px-4 py-3 text-left font-medium">Status</th>
                   <th className="px-4 py-3 text-left font-medium">Last updated</th>
+                  <th className="px-4 py-3 text-right font-medium">Actions</th>
                 </tr>
               </thead>
               <tbody className="stagger divide-y divide-border">
