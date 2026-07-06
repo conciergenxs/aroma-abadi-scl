e528cf10e1f1cb5ea10335a9120e7234deee4a12
Concierge NXS <concierge@nxs.id>
2026-07-06 18:39:57 +0700

    auto: src/routeTree.gen.ts [autodeploy]

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -159,6 +159,7 @@ export interface FileRoutesById {
   '/channels': typeof ChannelsRoute
   '/contacts': typeof ContactsRouteWithChildren
   '/inbox': typeof InboxRoute
+  '/promo-codes': typeof PromoCodesRoute
   '/settings': typeof SettingsRoute
   '/sku': typeof SkuRoute
   '/transactions': typeof TransactionsRoute
