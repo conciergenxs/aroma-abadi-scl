c0d95160fef2c95d42f0033915512644b520e25c
Concierge NXS <concierge@nxs.id>
2026-07-07 02:56:22 +0700

    auto: src/routes/contacts.tsx [autodeploy]

--- src/routes/contacts.tsx
+++ src/routes/contacts.tsx
@@ -495,9 +495,7 @@ function ContactsTable({
           {(isAllOrBrand || isMineView) && <th className={thCls}>Gender</th>}
           <th className={thCls}>WA Number</th>
           <th className={thCls}>Contact Type</th>
-          {isAllOrBrand && <th className={thCls}>Brands</th>}
           {isMineView && <th className={thCls}>Point Balance</th>}
-          {isMineView && <th className={thCls}>Brands</th>}
           {isMineView && <th className={thCls}>Last Transaction</th>}
           {isBaView && <>
             <th className={thCls}>Brand</th>
