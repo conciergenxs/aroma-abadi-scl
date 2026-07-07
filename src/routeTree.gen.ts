76d67b765109b92408ddd6ca04c49552d9909eee
Concierge NXS <concierge@nxs.id>
2026-07-07 13:59:06 +0700

    auto: src/routeTree.gen.ts [autodeploy]

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -149,7 +149,8 @@ export interface FileRoutesByFullPath {
   '/promo-codes/$promoId': typeof PromoCodesPromoIdRoute
   '/settings': typeof SettingsRoute
   '/sku': typeof SkuRoute
-  '/sku-detail/$skuId': typeof SkuSkuIdRoute
+  '/sku/$skuId': typeof SkuSkuIdRoute
+  '/sku-detail/$skuId': typeof SkuDetailSkuIdRoute
   '/transactions': typeof TransactionsRoute
   '/broadcasts/$broadcastId': typeof BroadcastsBroadcastIdRoute
   '/broadcasts/new': typeof BroadcastsNewRoute
