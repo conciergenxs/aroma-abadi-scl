a6405058b7ebfa9468bf52cca5cd046d6667fdcc
Concierge NXS <concierge@nxs.id>
2026-07-07 13:59:22 +0700

    auto: src/routeTree.gen.ts [autodeploy]

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -221,6 +221,7 @@ export interface FileRouteTypes {
     | '/promo-codes/$promoId'
     | '/settings'
     | '/sku'
+    | '/sku/$skuId'
     | '/sku-detail/$skuId'
     | '/transactions'
     | '/broadcasts/$broadcastId'
