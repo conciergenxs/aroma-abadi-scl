5c60bd8ea4e8a0eefbc35a6715766594ec0aeddb
Concierge NXS <concierge@nxs.id>
2026-08-06 06:19:57 +0700

    auto: src/routes/broadcasts.new.tsx [autodeploy]

--- src/routes/broadcasts.new.tsx
+++ src/routes/broadcasts.new.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, useNavigate, Link } from "@tanstack/react-router";
 import { AppShell } from "@/components/scl/app-shell";
 import { ChannelIcon } from "@/components/scl/channel-badge";
 import { TemplatePicker } from "@/components/scl/template-picker";
