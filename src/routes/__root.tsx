242ff7098a623413f746657d3af720efb417459c
Concierge NXS <concierge@nxs.id>
2026-08-12 13:46:09 +0700

    auto: src/routes/__root.tsx [autodeploy]

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,6 +13,7 @@ import { useRouterState } from "@tanstack/react-router";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "@/components/ui/sonner";
+import { LITE_MODE } from "@/lib/feature-flags";
 
 function NotFoundComponent() {
   return (
